7f83e62860b84272484ef401b5b2a7f88590a330
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-09-18 13:48:26 +0800

    Added Move Logic for Computer player

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -9,6 +9,8 @@ class Player {
 class Computer extends Player {
   gameboard = null;
   lastSuccessfulHitPosition = null;
+  conquerChaseAttempts = 0;
+  conquerChaseCount = 0;
 
   constructor() {
     super();
@@ -17,6 +19,11 @@ class Computer extends Player {
   }
 
   makeMove() {
+    if (!this.lastSuccessfulHitPosition) this.conqueringMove();
+    else randomMove();
+  }
+
+  randomMove() {
     let move = {
       x: Math.ceil(Math.random() * 9),
       y: Math.ceil(Math.random() * 9),
@@ -31,4 +38,63 @@ class Computer extends Player {
       this.lastSuccessfulHitPosition = move;
     }
   }
+
+  conqueringMove() {
+    let move = this.lastSuccessfulHitPosition();
+
+    count = this.conquerChaseAttempts;
+    while (this.gameboard.isMoveAvailable(move.x, move.y)) {
+      move = this.exploreAround(this.lastSuccessfulHitPosition, count);
+
+      count++;
+      if (count > 8) {
+        this.conquerChaseAttempts = 0;
+        this.conquerChaseCount = 0;
+        this.lastSuccessfulHitPosition = null;
+
+        this.randomMove();
+      }
+    }
+
+    let giveUpConquerChance = 0;
+
+    if (this.gameboard.receiveAttack(move.x, move.y)) {
+      this.conquerChaseAttempts = 0;
+      this.conquerChaseCount = 0;
+      this.lastSuccessfulHitPosition = move;
+      return;
+    }
+
+    if (this.conquerChaseCount > 0)
+      giveUpConquerChance =
+        10 + ((this.conquerChaseCount * (this.conquerChaseCount + 1)) / 2) * 5;
+    if (giveUpConquerChance < Math.random()) {
+      this.conquerChaseAttempts = 0;
+      this.conquerChaseCount = 0;
+      this.lastSuccessfulHitPosition = null;
+    }
+  }
+
+  exploreAround(move, index) {
+    const newMove = { x: move.x, y: move.y };
+
+    if (Math.floor(index / 3) === 1) {
+      newMove.x = move.x + 1;
+    } else if (Mathh.floor(index / 3) >= 2) {
+      newMove.x = move.x - 1;
+    }
+
+    if (index % 3 === 1) {
+      newMove.y = move.y + 1;
+    } else if (index % 3 >= 2) {
+      newMove.y = move.y - 1;
+    }
+
+    return newMove;
+  }
 }
+
+module.exports = {
+  Player: Player,
+  Computer: Computer,
+};
