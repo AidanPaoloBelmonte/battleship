b5ebf6fc2ed44d474a1186e30ab138f68a5d9c31
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-09-24 16:21:06 +0800

    feat: VS Computer button now starts a proper VS Computer game

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,17 +8,43 @@ const menu = document.querySelector("#main-menu");
 const p1Field = document.querySelector("#p1");
 const p2Field = document.querySelector("#p2");
 
-const p1Manager = new Player();
-const p2Manager = new Computer();
+let p1Manager = new Player();
+let p2Manager = new Computer();
 
 let watcher = null;
 
 let current_player = 1;
 
-function generateBoard(field, player) {
+function startComputerGame() {
+  p1Manager = new Player();
+  p2Manager = new Computer();
+
+  prepareBoard(p1Field, p1Manager);
+  prepareBoard(p2Field, p2Manager);
+
+  p1Field.classList.add("focus-field");
+}
+
+function prepareBoard(field, player) {
   const board = field.querySelector(".board");
 
+  generateBoard(board);
+
+  board.addEventListener("click", (e) => attack(e, player));
+
+  if (player instanceof Computer)
+    watcher = new ClassWatcher(field, "focus-field", computerTurn);
+  else watcher = null;
+}
+
+function generateBoard(board) {
   if (!board) return;
+
+  if (board.children) {
+    while (board.firstChild) {
+      board.removeChild(board.lastChild);
+    }
+  }
 
   for (let l = 0; l < 81; l++) {
     const cell = document.createElement("div");
@@ -27,12 +53,6 @@ function generateBoard(field, player) {
 
     board.appendChild(cell);
   }
-
-  board.addEventListener("click", (e) => attack(e, player));
-
-  if (player instanceof Computer)
-    watcher = new ClassWatcher(field, "focus-field", computerTurn);
-  else watcher = null;
 }
 
 function attack(e, player) {
@@ -74,10 +94,20 @@ function updateCell(cell, hit) {
 }
 
 window.onload = () => {
+  generateBoard(p1Field.querySelector(".board"));
+  generateBoard(p2Field.querySelector(".board"));
+
   menu.showModal();
-
-  generateBoard(p1Field, p1Manager);
-  generateBoard(p2Field, p2Manager);
-
-  p1Field.classList.add("focus-field");
+  console.log("Wow");
 };
+
+menu.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("menu-button")) return;
+  const id = e.target.id;
+
+  console.log(id === "vs-com");
+  if (id === "vs-com") {
+    startComputerGame();
+    menu.close();
+  }
+});
